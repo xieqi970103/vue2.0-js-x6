dee4e3ce9cb062130391e551a7795a744837c7b4
2020401 <xieqi@dianchu.com>
2022-02-03 16:48:56 +0800

    feat: 🎸 初始化画布

--- src/views/x6/index.vue
+++ src/views/x6/index.vue
@@ -1,22 +1,75 @@
-<!--
- * @Description: 
- * @Author: xxqq
- * @Date: 2022-01-31 17:58:07
- * @LastEditTime: 2022-02-01 18:31:49
- * @LastEditors: xxqq
--->
 <template>
-  <a-card class="panel"> 123 </a-card>
+  <a-card class="panel">
+    <div class="panel-content-wrapper">
+      <!-- 左侧节点选择区 -->
+      <!-- 中部内容区 -->
+      <div class="content-wrapper">
+        <div class="draw-wrapper" id="drag-container"></div>
+      </div>
+      <!-- 右侧节点配置区 -->
+    </div>
+  </a-card>
 </template>
 
 <script>
+import "@antv/x6-vue-shape";
+import { Graph } from "@antv/x6";
 export default {
   name: "X6",
+  data() {
+    return {
+      graph: null,
+    };
+  },
+  mounted() {
+    this.initGraph();
+  },
+  methods: {
+    // 初始化
+    initGraph() {
+      const that = this;
+      that.graph = new Graph({
+        container: document.getElementById("drag-container"), // 设置画布的容器
+        // width： 1600 // 默认使用容器的宽高
+        // height：900
+        // 网格
+        grid: {
+          size: 10, // 网格大小 10px
+          type: "doubleMesh",
+          args: [
+            {
+              color: "#eee", // 主网格线颜色
+              thickness: 1, // 主网格线宽度
+            },
+            {
+              color: "#ddd", // 次网格线颜色
+              thickness: 1, // 次网格线宽度
+              factor: 4, // 主次网格线间隔
+            },
+          ],
+          visible: true, // 渲染网格背景
+        },
+      });
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
+::v-deep {
+  .ant-card-body {
+    width: 95vw;
+    height: 95vh;
+  }
+}
 .panel {
-  width: 95vw;
-  height: 95vh;
+  .panel-content-wrapper {
+    height: 100%;
+    .content-wrapper {
+      height: 100%;
+      .draw-wrapper {
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
